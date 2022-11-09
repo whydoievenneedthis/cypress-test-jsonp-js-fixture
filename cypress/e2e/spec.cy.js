c2c664a533e1c68aa504245ae8b07e91719d0dbc
whydoievenneedthis <firefoxpdm@gmail.com>
2022-11-09 22:30:11 +0100

    Intercept jsonp with js fixture

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,8 @@
-/// <reference types="cypress" />
 describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+  it('does not work', () => {
+    cy.intercept(
+      { method: 'GET', url: 'https://code.jquery.com/jquery-3.6.1.min.js' },
+      { fixture: 'jquery.js' }
+    );
+  });
+});
